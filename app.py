99ae0c0a9c301cc2dd6d08da1227981df23556a8
Adam King <adam.king0126@gmail.com>
2021-12-16 06:36:21 -0600

    Enable debug mode

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def repos():
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
     app.secret_key = app.config.get("SECRET_KEY")
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
